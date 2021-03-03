b1006ba3c145fc0aba2867e1e4d9a38d4c5daa8f
yu277777 <chocolate27836@gmail.com>
2021-03-03 19:47:44 +0800

    Merge branch 'hehehehehe' of https://github.com/YU277777/haha into hehehehehe
    # Conflicts:
    #	HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -6,11 +6,17 @@ rubbish
     public static void main(String[]args){
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 
         System.out.println("¤j®a¦n¡A§Ú¦n¾j³á");
 		System.out.println("¤j®a¦n¡A§Ú¦n¾j");
 			System.out.println("¤j®a¦n¡A§Ú¯uªº¦n¾jªü");
 
+=======
+        System.out.println("å¤§å®¶å¥½ï¼Œæˆ‘å¥½é¤“å–”");
+		System.out.println("å¤§å®¶å¥½ï¼Œæˆ‘å¥½é¤“");
+			System.out.println("å¤§å®¶å¥½ï¼Œæˆ‘çœŸçš„å¥½é¤“å–”å–”");
+>>>>>>> cf362bc6990cdf1c3c8da086b3c338adbc11bd0e
 
 
     }
